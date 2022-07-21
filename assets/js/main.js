8082126834bac49a01de047faa7a43e1cf79d9b5
Dima Hrachev <babun5678@gmail.com>
2022-07-21 12:45:13 +0300

    last updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,3 +20,52 @@ const tl = TweenMax.staggerFrom(
   { y: '100%', ease: Power2.easeInOut, yoyo: true },
   0.05,
 );
+
+// iframe video from youtube start
+
+const tag = document.createElement('script');
+
+tag.src = 'https://www.youtube.com/iframe_api';
+const firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+let player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+    height: '1080',
+    width: '1920',
+    controls: 0,
+    videoId: 'hVN3cc4ZWpg',
+    events: {
+      onStateChange: onPlayerStateChange,
+    },
+  });
+}
+function pauseVideo() {
+  player.pauseVideo();
+  console.log('stoped');
+}
+
+function onPlayerStateChange(event) {
+  if (event.data == YT.PlayerState.PAUSED) {
+    $('.video-section__player').removeClass('active');
+  }
+}
+
+function playVideo() {
+  console.log('videoPlay');
+  $('.video-section__player').addClass('active');
+  player.playVideo();
+}
+
+$('.video-section__player').click(function () {
+  playVideo();
+  $('html,body').animate(
+    {
+      scrollTop: $('.video-section__player').offset().top + 2,
+    },
+    'slow',
+  );
+});
+
+// iframe video from youtube end
