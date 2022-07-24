85e81cca30c76655d6d5b9942877f6c0d1330d42
Ivan Shalnov <ivan.shalnov.94@gmail.com>
2022-07-25 00:58:20 +0200

    turn of follow

--- assets/js/btn-follow-cursor.js
+++ assets/js/btn-follow-cursor.js
@@ -1,25 +1,25 @@
-const areaRef = document.querySelector('.video-section__player');
-const btnRef = areaRef.querySelector('.play-btn');
-function throttle(func, interval) {
-  var lastCall = 0;
-  return function () {
-    var now = Date.now();
-    if (lastCall + interval < now) {
-      lastCall = now;
-      return func.apply(this, arguments);
-    }
-  };
-}
+// const areaRef = document.querySelector('.video-section__player');
+// const btnRef = areaRef.querySelector('.play-btn');
+// function throttle(func, interval) {
+//   var lastCall = 0;
+//   return function () {
+//     var now = Date.now();
+//     if (lastCall + interval < now) {
+//       lastCall = now;
+//       return func.apply(this, arguments);
+//     }
+//   };
+// }
 
-const box = btnRef.getBoundingClientRect();
-const boxX = box.left + window.pageXOffset + box.width;
-const boxY = box.top + window.pageYOffset + box.height;
-function moveBtn(event) {
-  const onX = event.pageX - boxX;
-  const onY = event.pageY - boxY;
-  if (onX !== 0 && onY !== 0) {
-    btnRef.style.transform = `translate(${onX}px, ${onY}px)`;
-  }
-}
+// const box = btnRef.getBoundingClientRect();
+// const boxX = box.left + window.pageXOffset + box.width;
+// const boxY = box.top + window.pageYOffset + box.height;
+// function moveBtn(event) {
+//   const onX = event.pageX - boxX;
+//   const onY = event.pageY - boxY;
+//   if (onX !== 0 && onY !== 0) {
+//     btnRef.style.transform = `translate(${onX}px, ${onY}px)`;
+//   }
+// }
 
-areaRef.addEventListener('mousemove', moveBtn);
+// areaRef.addEventListener('mousemove', moveBtn);
