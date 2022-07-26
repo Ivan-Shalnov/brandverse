672a27a0a17e4efd73490ce2e91286d4c4cde110
Dima Hrachev <babun5678@gmail.com>
2022-07-27 00:13:04 +0300

    fixies

--- assets/js/main.js
+++ assets/js/main.js
@@ -77,6 +77,19 @@ $('.marquee').marquee({
   duplicated: true,
 });
 
+$('.marquee-right').marquee({
+  //speed in milliseconds of the marquee
+  duration: 10000,
+  //gap in pixels between the tickers
+  gap: 20,
+  //time in milliseconds before the marquee will start animating
+  delayBeforeStart: 0,
+  //'left' or 'right'
+  direction: 'right',
+  //true or false - should the marquee be duplicated to show an effect of continues flow
+  // duplicated: true,
+});
+
 gsap.registerPlugin(ScrollTrigger);
 
 const locoScroll = new LocomotiveScroll({
